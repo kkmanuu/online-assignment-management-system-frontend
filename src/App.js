496474c4dbbd7b1ab359584b90ca630e583249bf
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-03-09 03:07:25 +0300

    feat: integrate routes in App.js with AuthProvider and Navbar for role-based access

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from './pages/authentications/Signup';
-import Login from './pages/authentications/Login';
-import Navbar from './components/Navbar';
-import ForgotPassword from './pages/authentications/ForgotPassword';
+
+import ForgotPassword from "./pages/authentications/ForgotPassword";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./pages/PrivateRoute";
+import StudentDashboard from "./pages/StudentsDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import Login from "./pages/authentications/Login";
+import Signup from "./pages/authentications/Signup";
 
 function App() {
   return (
-    <Router>
-    <Navbar/>
-    <Routes>
-    {/* <Route path="/" element={<Home />} /> */}
-      <Route path="/" element={<Signup />} /> 
-       <Route path="/login" element={<Login />}/>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-     
-    </Routes>
-  </Router>
+    <AuthProvider>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+
+            {/* Protected Routes */}
+            <Route
+              path="/student-dashboard"
+              element={
+                <PrivateRoute role="student">
+                  <StudentDashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/admin-dashboard"
+              element={
+                <PrivateRoute role="admin">
+                  <AdminDashboard />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </>
+      </Router>
+    </AuthProvider>
   );
 }
 
