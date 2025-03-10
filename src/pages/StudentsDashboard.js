1ac2e699696e315ea2f7bf4621ff36295505e7f8
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-03-10 14:32:39 +0300

    refactor: Improve StudentDashboard structure and readability

--- src/pages/StudentsDashboard.js
+++ src/pages/StudentsDashboard.js
@@ -1,7 +1,15 @@
 import React from "react";
+import DashboardLayout from "../components/DashboardLayout";
+import { Typography } from "@mui/material";
 
 const StudentDashboard = () => {
-  return <div>Student Dashboard</div>;
+  return (
+    <DashboardLayout>
+      <Typography variant="h4" gutterBottom>
+        Student Dashboard
+      </Typography>
+    </DashboardLayout>
+  );
 };
 
 export default StudentDashboard;
